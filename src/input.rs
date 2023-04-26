6f7fb47e955ec8db369c128e635f00fbcb732208
William Pederzoli <wpederzoli@gmail.com>
2023-04-26 18:51:18 +0200

    move speed to constants

--- src/input.rs
+++ src/input.rs
@@ -2,6 +2,9 @@ use bevy::prelude::{Input, KeyCode, Query, Res, Transform, With};
 
 use crate::snake::PlayerMovement;
 
+const ANGUILA_SPEED: f32 = 1.0;
+const DIAGONAL_SPEED: f32 = ANGUILA_SPEED * 0.75;
+
 pub fn move_snake(
     key: Res<Input<KeyCode>>,
     mut query: Query<&mut Transform, With<PlayerMovement>>,
@@ -9,30 +12,30 @@ pub fn move_snake(
     let mut snake = query.single_mut();
     if key.pressed(KeyCode::Left) {
         if key.pressed(KeyCode::Up) || key.pressed(KeyCode::Down) {
-            snake.translation.x -= 0.75;
+            snake.translation.x -= DIAGONAL_SPEED;
         } else {
-            snake.translation.x -= 1.0;
+            snake.translation.x -= ANGUILA_SPEED;
         }
     }
     if key.pressed(KeyCode::Right) {
         if key.pressed(KeyCode::Up) || key.pressed(KeyCode::Down) {
-            snake.translation.x += 0.75;
+            snake.translation.x += DIAGONAL_SPEED;
         } else {
-            snake.translation.x += 1.0;
+            snake.translation.x += ANGUILA_SPEED;
         }
     }
     if key.pressed(KeyCode::Up) {
         if key.pressed(KeyCode::Left) || key.pressed(KeyCode::Right) {
-            snake.translation.y += 0.75
+            snake.translation.y += DIAGONAL_SPEED;
         } else {
-            snake.translation.y += 1.0;
+            snake.translation.y += ANGUILA_SPEED;
         }
     }
     if key.pressed(KeyCode::Down) {
         if key.pressed(KeyCode::Left) || key.pressed(KeyCode::Right) {
-            snake.translation.y -= 0.75
+            snake.translation.y -= DIAGONAL_SPEED;
         } else {
-            snake.translation.y -= 1.0;
+            snake.translation.y -= ANGUILA_SPEED;
         }
     }
 }
