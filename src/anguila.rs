bd2c86cd7125db06d88becf20e4df2154869aa0c
William Pederzoli <wpederzoli@gmail.com>
2023-04-27 13:32:28 +0200

    Collision detection for targets

--- src/anguila.rs
+++ src/anguila.rs
@@ -1,7 +1,12 @@
 use bevy::{
-    prelude::{default, Color, Commands, Component, Transform, Vec3},
+    prelude::{default, Color, Commands, Component, Entity, Query, Transform, Vec3, With},
     sprite::{Sprite, SpriteBundle},
 };
+
+use crate::targets::{Target, TARGET_HEIGHT, TARGET_WIDTH};
+
+pub const ANGUILA_WIDTH: f32 = 20.0;
+pub const ANGUILA_HEIGHT: f32 = 20.0;
 
 #[derive(Component)]
 pub struct PlayerMovement;
@@ -15,7 +20,7 @@ pub fn setup_anguila(mut commands: Commands) {
                     ..default()
                 },
                 transform: Transform {
-                    scale: Vec3::new(20.0, 20.0, 0.0),
+                    scale: Vec3::new(ANGUILA_WIDTH, ANGUILA_HEIGHT, 0.0),
                     ..default()
                 },
                 ..default()
@@ -23,4 +28,29 @@ pub fn setup_anguila(mut commands: Commands) {
         },
         PlayerMovement,
     ));
+}
+
+pub fn target_collision(
+    mut commands: Commands,
+    mut targets: Query<(Entity, &Transform, &Target)>,
+    player: Query<&Transform, With<PlayerMovement>>,
+) {
+    let anguila = player.single();
+
+    for (entity, position, _) in &mut targets {
+        if is_colliding(position.translation, anguila.translation) {
+            commands.entity(entity).remove::<SpriteBundle>();
+        }
+    }
+}
+
+fn is_colliding(target: Vec3, anguila: Vec3) -> bool {
+    if anguila.x + ANGUILA_WIDTH / 2.0 >= target.x - TARGET_WIDTH / 2.0
+        && anguila.y + ANGUILA_HEIGHT / 2.0 >= target.y - TARGET_HEIGHT / 2.0
+        && anguila.x - ANGUILA_WIDTH / 2.0 <= target.x + TARGET_WIDTH / 2.0
+        && anguila.y - ANGUILA_HEIGHT / 2.0 <= target.y + TARGET_HEIGHT / 2.0
+    {
+        return true;
+    }
+    false
 }
