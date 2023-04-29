d7e7640243a3ee680b47a70948cbb11a33de77dc
William Pederzoli <wpederzoli@gmail.com>
2023-04-29 16:31:05 +0200

    Refactor how we're getting player_pos and direction in collision

--- src/collision.rs
+++ src/collision.rs
@@ -1,26 +1,26 @@
 use bevy::{
-    prelude::{Commands, Entity, Query, Transform, Vec3},
+    prelude::{Commands, Entity, Query, Transform, Vec3, With},
     sprite::SpriteBundle,
 };
 
 use crate::{
     anguila::{Anguila, Direction, ANGUILA_HEIGHT, ANGUILA_WIDTH},
-    segment::add_segment,
+    segment::{add_segment, Segment},
     targets::{Target, TARGET_HEIGHT, TARGET_WIDTH},
 };
 
 pub fn target_collision(
     mut commands: Commands,
     mut targets: Query<(Entity, &Transform, &Target)>,
-    mut player: Query<(&Transform, &Direction, &Anguila)>,
+    mut player: Query<(&Transform, &Direction), With<Anguila>>,
+    mut segments: Query<&mut Segment>,
 ) {
-    for (player_pos, player_dir, _) in &mut player {
-        for (entity, target_pos, _) in &mut targets {
-            if is_colliding(&target_pos.translation, &player_pos.translation) {
-                commands.entity(entity).remove::<SpriteBundle>();
-                commands.entity(entity).remove::<Target>();
-                add_segment(&mut commands, &player_pos.translation, &player_dir.0);
-            }
+    let (player_pos, player_dir) = player.single_mut();
+    for (entity, target_pos, _) in &mut targets {
+        if is_colliding(&target_pos.translation, &player_pos.translation) {
+            commands.entity(entity).remove::<SpriteBundle>();
+            commands.entity(entity).remove::<Target>();
+            add_segment(&mut commands, &player_pos.translation, &player_dir.0);
         }
     }
 }
